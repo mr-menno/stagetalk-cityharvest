ae83f8fb6d6d2ce11373e4a9b186752d121a110d
Menno V <menno@vanderlist.ca>
2024-12-21 19:57:52 -0800

    team card

--- packages/web/src/routes/profile.lazy.tsx
+++ packages/web/src/routes/profile.lazy.tsx
@@ -1,11 +1,17 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { createLazyFileRoute } from "@tanstack/react-router";
+import { useQuery } from "convex/react";
+import { api } from "shared/convex/_generated/api";
+import useStagetalkStore from "shared/stagetalk-store";
 
 export const Route = createLazyFileRoute("/profile")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const { teamId } = useStagetalkStore();
+  const team = useQuery(api.teams.getTeam, teamId ? { teamId } : "skip");
+
   return (
     <div className="flex flex-col h-dvh overscroll-none">
       <div className="w-full bg-primary text-primary-foreground font-semibold p-2 flex flex-row">
@@ -18,12 +24,19 @@ function RouteComponent() {
           </CardHeader>
           <CardContent>Your profile...</CardContent>
         </Card>
-        <Card>
-          <CardHeader>
-            <CardTitle>Your Team</CardTitle>
-          </CardHeader>
-          <CardContent>Your Team...</CardContent>
-        </Card>
+        {team && (
+          <Card>
+            <CardHeader>
+              <CardTitle>Team: {team.name}</CardTitle>
+            </CardHeader>
+            <CardContent className="flex flex-col">
+              <div>
+                <strong>Invite Code:</strong>{" "}
+                <span className="font-mono">{team.inviteCode}</span>
+              </div>
+            </CardContent>
+          </Card>
+        )}
       </div>
     </div>
   );
